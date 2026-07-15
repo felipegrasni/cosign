344a39ed05afa5503858aac90a0f8d69e769c140
felipegrasni <felipegrasni@gmail.com>
2026-07-15 13:03:50 -0300

    Add robots host metadata

--- app/robots.ts
+++ app/robots.ts
@@ -1,3 +1,10 @@
 import type { MetadataRoute } from "next";
 import { publicEnv } from "@/lib/env";
-export default function robots(): MetadataRoute.Robots { return { rules: { userAgent: "*", allow: "/" }, sitemap: `${publicEnv.appUrl}/sitemap.xml` }; }
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    host: publicEnv.appUrl,
+    sitemap: `${publicEnv.appUrl}/sitemap.xml`
+  };
+}
